341c1243267d3d3797bd0e2b6a9dca78d29bb355
Christian Rupp <privat@christian-rupp.de>
2015-04-08 19:30:04 +0200

    osClasses finished.

---
+++ Python/ops.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class ops(Enum):
+	XP = ("XP", 5.1, "WINDOWS")
+
+	def __init__(self):
+		pass
--- Python/osClasses.py
+++ Python/osClasses.py
@@ -1,5 +1,14 @@
 from enum import Enum
+import re
 
 class osClasses(Enum):
 	DESKTOP = 1
 	SERVER = 2
+
+	def parse(self, string):
+		if re.search(osClasses.DESKTOP.name, string, re.IGNORECASE):
+			return osClasses.DESKTOP
+		elif re.search(osClasses.SERVER.name, string, re.IGNORECASE):
+			return osClasses.SERVER
+		else:
+			return None
