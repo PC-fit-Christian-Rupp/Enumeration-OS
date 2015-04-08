a5bda4cccc2c7dba845bba90340b44356a383fdc
Christian Rupp <privat@christian-rupp.de>
2015-04-08 20:42:04 +0200

    All Windows Desktop Systems added.

--- Python/ops.py
+++ Python/ops.py
@@ -6,7 +6,9 @@ class ops(Enum):
 	# WINDOWS Desktop Family
 	XP = ("XP", 5.1, osFamily.osFamily.WINDOWS)
 	VISTA = ("Vista", 6.0, osFamily.osFamily.WINDOWS)
-	EIGHT = ("7", 6.1, osFamily.osFamily.WINDOWS)
+	SEVEN = ("7", 6.1, osFamily.osFamily.WINDOWS)
+	EIGHT = ("8", 6.2, osFamily.osFamily.WINDOWS)
+	EIGHTONE = ("8.1", 6.3, osFamily.osFamily.WINDOWS)
 
 	def getAllClasses(self):
 		pass
